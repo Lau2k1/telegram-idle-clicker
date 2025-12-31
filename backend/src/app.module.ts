62d059c561f385a8694c257a1045a5fdf2b3f5b4
Lau2k1 <nikitin_danechka@bk.ru>
2025-12-31 12:53:12 +0300

    update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-import { GameController } from './game/game.controller';
+import { GameModule } from './game/game.module'; // Добавьте импорт модуля
 
 @Module({
-  controllers: [GameController],
+  imports: [GameModule], // Импортируйте GameModule здесь
+  controllers: [],       // Контроллеры теперь будут подтягиваться из GameModule
+  providers: [],
 })
 export class AppModule {}
