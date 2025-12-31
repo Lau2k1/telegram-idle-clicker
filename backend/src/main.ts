9f0e2fdcde9b4d7f17d2ea4580532bbcc5689f60
Lau2k1 <nikitin_danechka@bk.ru>
2025-12-31 17:41:53 +0300

    update gameService for bigInt in string

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Это ГЛОБАЛЬНОЕ исправление для BigInt
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.enableCors({
-    origin: '*',
+    origin: '*', // Разрешаем фронтенду доступ
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
