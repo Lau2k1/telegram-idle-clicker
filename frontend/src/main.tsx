f6d66bd9144cca7669a8c0bd4f3e6fb7eea7d061
Lau2k1 <nikitin_danechka@bk.ru>
2026-01-07 12:30:54 +0300

    fix css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles.css";
+import "./main.css";
 import App from "./App";
 
 // Удаляем лишние вызовы здесь, так как они теперь внутри App.tsx
